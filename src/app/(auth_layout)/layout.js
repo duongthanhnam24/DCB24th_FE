903fdb32aa6fec36f1f1094b8e3cef2bd4a2fe8c
duongthanhnam24 <namthanh2412@gmail.com>
2023-10-10 02:55:19 +0700

    fix metadata

--- src/app/(auth_layout)/layout.js
+++ src/app/(auth_layout)/layout.js
@@ -2,11 +2,6 @@
 
 import "@/styles/globals.css";
 
-export const metadata = {
-    title: "Authenticate",
-    description: "Sign In",
-};
-
 export default function AuthLayout({ children }) {
     return <div className="authentication">{children}</div>;
 }
