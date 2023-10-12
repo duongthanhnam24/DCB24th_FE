46f3235870a15516cf7b4eff1bdd47a67f9d45d8
duongthanhnam24 <namthanh2412@gmail.com>
2023-10-12 12:50:22 +0700

    12/10 new key

--- src/components/table/tableOrder.js
+++ src/components/table/tableOrder.js
@@ -66,9 +66,9 @@ function TableOrder() {
                     </tr>
                 </thead>
                 <tbody>
-                    {data?.map((item) => {
+                    {data?.map((item, i) => {
                         return (
-                            <tr>
+                            <tr key={i}>
                                 <th
                                     scope="row"
                                     className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
